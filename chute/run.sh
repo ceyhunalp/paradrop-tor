0f31bde8b3faab584ae327a486f8c983e9dba295
ecacs736 <cipetpet5@yandex.com>
2016-10-13 20:04:55 -0500

    launching with iptables config

--- chute/run.sh
+++ chute/run.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 # Get wlan0 IP addr
-wlanAddr = $(ifconfig -a | grep 'inet addr:192.168' | awk '{print $2}' | awk -F':' '{print $2}')
+
+wlanAddr=$(ifconfig -a | grep 'inet addr:192.168' | awk '{print $2}' | awk -F':' '{print $2}')
 
 # Write tor config
+echo "Log notice file /var/log/tor/notices.log" >> /etc/tor/torrc
+echo "VirtualAddrNetwork 10.192.0.0/10" >> /etc/tor/torrc
+echo "AutomapHostsSuffixes .onion,.exit" >> /etc/tor/torrc
+echo "AutomapHostsOnResolve 1" >> /etc/tor/torrc
+echo "TransPort 9040" >> /etc/tor/torrc
+echo "TransListenAddress $wlanAddr" >> /etc/tor/torrc
+echo "DNSPort 53" >> /etc/tor/torrc
+echo "DNSListenAddress $wlanAddr" >> /etc/tor/torrc
 
+#iptables configuration
+
+iptables -F
+iptables -t nat -F
+iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 22
+iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
 
 # Redirect HTTP traffic to the proxy.
 #iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080
@@ -16,9 +32,9 @@ wlanAddr = $(ifconfig -a | grep 'inet addr:192.168' | awk '{print $2}' | awk -F'
 #service privoxy force-reload
 #service dansguardian start
 
-while true; do
-    sleep 300
-done
+#while true; do
+#    sleep 300
+#done
 
 # If execution reaches this point, the chute will stop running.
 exit 0
