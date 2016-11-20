ab598ecfc2e87b14e1435b48b1c8da0079940d31
ceyhunalp <e.ceyhun.alp@gmail.com>
2016-11-20 01:17:56 -0600

    Modified run.sh -- testing hidden service access

--- chute/run.sh
+++ chute/run.sh
@@ -16,15 +16,22 @@ echo "AutomapHostsOnResolve 1" >> /etc/tor/torrc
 echo "TransPort 9040" >> /etc/tor/torrc
 echo "TransListenAddress $wlanAddr" >> /etc/tor/torrc
 echo "DNSPort 53" >> /etc/tor/torrc
-echo "DNSListenAddress $wlanAddr" >> /etc/tor/torrc
+#DNSListenAddress is deprecated?
+#echo "DNSListenAddress $wlanAddr" >> /etc/tor/torrc
 
-#iptables configuration
+# Modify /etc/resolv.conf so that DNS queries go through Tor
 
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+
+# iptables configuration
 iptables -F
 iptables -t nat -F
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 22
 iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
+
+# Transparently redirect any traffic destined for the Tor virtual address space through the Tor transport port we designated above
+iptables -t nat -A OUTPUT -p tcp -d 10.192.0.0./10 -j REDIRECT --to-ports 9040
 
 # Redirect HTTP traffic to the proxy.
 iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080
