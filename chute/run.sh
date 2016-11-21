527f5c0effe14c68e21688473e84a851a55fa539
ceyhunalp <e.ceyhun.alp@gmail.com>
2016-11-20 18:37:38 -0600

    still playing around

--- chute/run.sh
+++ chute/run.sh
@@ -34,8 +34,8 @@ iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 5353 -j REDIRECT --to-port
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
 
 # Transparently redirect any traffic destined for the Tor virtual address space through the Tor transport port we designated above
-#iptables -A INPUT -p tcp --dport 9040 -j ACCEPT
-#iptables -t nat -A PREROUTING -i wlan0 -p tcp -d 10.192.0.0/11 -j REDIRECT --to-port 9040
+iptables -A INPUT -p tcp --dport 9040 -j ACCEPT
+iptables -t nat -A PREROUTING -i wlan0 -p tcp -d 10.192.0.0/11 -j REDIRECT --to-port 9040
 iptables -t nat -A OUTPUT -p tcp -d 10.192.0.0./10 -j REDIRECT --to-ports 9040
 
 # Redirect HTTP traffic to the proxy.
