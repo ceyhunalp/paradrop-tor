3c0cce33397eae57eb5d5ffff59f88c61d26f0bb
ceyhunalp <e.ceyhun.alp@gmail.com>
2016-12-03 19:08:33 -0600

    postrouting rule

--- chute/run.sh
+++ chute/run.sh
@@ -15,7 +15,7 @@ echo "AutomapHostsSuffixes .onion,.exit" >> /etc/tor/torrc
 echo "AutomapHostsOnResolve 1" >> /etc/tor/torrc
 echo "TransPort 9040" >> /etc/tor/torrc
 echo "TransListenAddress $wlanAddr" >> /etc/tor/torrc
-echo "DNSPort 53" >> /etc/tor/torrc
+echo "DNSPort 5353" >> /etc/tor/torrc
 #echo "DNSListenAddress $wlanAddr" >> /etc/tor/torrc
 
 # Modify /etc/resolv.conf so that DNS queries go through Tor
@@ -27,14 +27,12 @@ echo "nameserver 127.0.0.1" >> /etc/resolv.conf
 iptables -F
 iptables -t nat -F
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 22
-iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
+#iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
+iptables -t nat -A POSTROUTING -o eth0 -p udp --dport 53 -j REDIRECT --to-ports 5353
 
 # Transparently redirect any traffic destined for the Tor virtual address space through the Tor transport port we designated above
-#iptables -A INPUT -p tcp --dport 9040 -j ACCEPT
-#iptables -t nat -A PREROUTING -i wlan0 -p tcp -d 10.192.0.0/10 -j REDIRECT --to-port 9040
 iptables -t nat -A OUTPUT -p tcp -d 10.192.0.0/10 -j REDIRECT --to-ports 9040
-#iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5353
 
 # Redirect HTTP traffic to the proxy.
 iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080
