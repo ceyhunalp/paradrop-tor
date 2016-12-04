60c95423208e355e88a1919b58287d8ae08fa150
ceyhunalp <e.ceyhun.alp@gmail.com>
2016-12-03 23:29:29 -0600

    return to original config

--- chute/run.sh
+++ chute/run.sh
@@ -20,20 +20,20 @@ echo "DNSPort 53" >> /etc/tor/torrc
 
 # Modify /etc/resolv.conf so that DNS queries go through Tor
 
-cp /dev/null /etc/resolv.conf
-echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+#cp /dev/null /etc/resolv.conf
+#echo "nameserver 127.0.0.1" >> /etc/resolv.conf
 
 # iptables configuration
 iptables -F
 iptables -t nat -F
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-ports 22
-#iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
 
 #iptables -t nat -A POSTROUTING -o eth0 -p udp --dport 53 -j REDIRECT --to-ports 5353
 
 # Transparently redirect any traffic destined for the Tor virtual address space through the Tor transport port we designated above
-iptables -t nat -A OUTPUT -p tcp -d 10.192.0.0/10 -j REDIRECT --to-ports 9040
+#iptables -t nat -A OUTPUT -p tcp -d 10.192.0.0/10 -j REDIRECT --to-ports 9040
 
 # Redirect HTTP traffic to the proxy.
 iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080
